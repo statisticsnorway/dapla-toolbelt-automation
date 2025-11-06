40145fbb56038a8a330fc2cc166460d500003990
skykanin <3789764+skykanin@users.noreply.github.com>
2025-11-06 10:18:00 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Provide development environment for dapla-toolbelt-automation";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        self',
+        ...
+      }: let
+        inherit (pkgs) python313Packages;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "dapla-toolbelt-automation devel";
+          packages = [
+            pkgs.pre-commit
+            (pkgs.python313.withPackages (ps: [ps.pre-commit-hooks]))
+            python313Packages.black
+            python313Packages.ruff
+            pkgs.uv
+          ];
+        };
+        formatter = pkgs.alejandra;
+        packages.darglint = python313Packages.buildPythonPackage rec {
+          pname = "darglint";
+          version = "1.8.1";
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-CV6dKPQpWJzJ5WvG+4RJxG3vEj7PbLzZXXgTdGHc6Ts=";
+          };
+          format = "setuptools";
+          doCheck = false;
+        };
+      };
+    };
+}
